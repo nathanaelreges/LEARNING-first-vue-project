e14aefbca5e09420664b7f2c581719d5bac79fd0
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-23 15:17:07 -0300

    first print

---
+++ src/views/Cycles.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Cycles">
+    Welcome to Billing Cycles App
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+/*
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}*/
+</script>
