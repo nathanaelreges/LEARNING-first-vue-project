9f90a106d2f85308966c1b509fb73dffcf7f984f
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-23 16:20:02 -0300

    Adding cycles

--- src/components/CyclesList.vue
+++ src/components/CyclesList.vue
@@ -1,11 +1,25 @@
 <template>
 <div>
    <div class="card text-center">
-    <ul class="list-group list-group-flush">
-      <li v-for="item in list" :key="item.id" class="list-group-item">
-        {{ item.name }}
-      </li>
-    </ul>  
+      <div class="card-header">
+         <h4>Cycles</h4> 
+      </div>
+      <div class="card-body">
+         <ul class="list-group list-group-flush">
+            <li v-for="item in list" :key="item.id" class="list-group-item">
+               {{ item.name }}
+            </li>
+         </ul>  
+      </div>
+      <div class="card-footer">
+         <div class="input-group">
+            <input v-model="newCycleName" type="text" class="form-control" 
+               placeholder="Type new Cycle" aria-label="Type a new Cycle" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+               <button class="btn btn-primary" type="button" @click="addCycle">Add</button>
+            </div>
+         </div>
+      </div>
    </div>
 </div>
 </template>
@@ -15,17 +29,27 @@ export default {
    name: "CyclesList",
    data() {
       return {
+         newCycleName: '',
          list: [
-         { name: "Janeiro" },
-         { name: "Fevereiro" },
-         { name: "Março" },
-         { name: "Abril" }
-         ],
-         name: "arroz"
+            { name: "Janeiro" },
+            { name: "Fevereiro" },
+            { name: "Março" },
+            { name: "Abril" }
+         ]
+      }
+   },
+   methods: {
+      addCycle () {
+         this.list.push({name: this.newCycleName})
       }
    }
 }
 </script>
 
 <style scoped>
+
+.list-group-item {
+   border: none;
+}
+
 </style>  
